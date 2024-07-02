60b9b2a6d2a592c004f5e2015668a92032de8dd5
Sanae <sanae.s.soma@gmail.com>
2024-07-01 20:18:36 -1000

    add pipeline setting in uno.config.js

--- uno.config.js
+++ uno.config.js
@@ -5,6 +5,18 @@ import {
 } from 'unocss';
 
 export default defineConfig({
+  content: {
+    pipeline: {
+      include: [
+        // the default
+        /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/,
+        // include js/ts files
+        'src/**/*.{js,ts}',
+      ],
+      // exclude files
+      // exclude: []
+    },
+  },
   presets: [
     presetAttributify(),
     presetUno()
