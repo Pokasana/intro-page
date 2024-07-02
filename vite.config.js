19315355eedd311f81d46657fe09844bf55ad4dd
Sanae <sanae.s.soma@gmail.com>
2024-07-01 20:16:34 -1000

    capitalize import from @vitejs/plugin-react to standardize format of plugins

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import UnoCSS from 'unocss/vite'
-import react from '@vitejs/plugin-react'
+import React from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     UnoCSS(),
-    react()
+    React()
   ],
 })
