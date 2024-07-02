1413ed7bd4fc9565f509f9588c747d8341d2b399
Sanae <sanae.s.soma@gmail.com>
2024-07-01 21:50:28 -1000

    add comment to disable eslint for React import in NavBar.js to ignore no-unused-vars error

---
+++ src/app/NavBar.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <div id="nav_bar">
+      <NavLink id="nav_link" to="/items">
+        Items
+      </NavLink>
+      <NavLink id="nav_link" to="/users">
+        Users
+      </NavLink>
+      <NavLink id="nav_link" to="/shops">
+        Shops
+      </NavLink>
+    </div>
+  );
+}
+
+export default NavBar;
