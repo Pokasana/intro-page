9f5acf215b6feec92851cd3b5d4605eaedd73cec
Sanae <sanae.s.soma@gmail.com>
2024-07-08 23:50:02 -1000

    add initial content in Info.jsx

---
+++ src/features/cv/Info.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Info() {
+  return (
+    <div>
+      <h1>Sanae Shiromoto</h1>
+      <p>Software Engineer</p>
+      <p>Honolulu, Hawaii, United States</p>
+      <a>Mail</a>
+      <a>GitHub</a>
+      <a>LinkedIn</a>
+    </div>
+  )
+}
+
+export default Info;
