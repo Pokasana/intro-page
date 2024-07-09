22128bac3ef006dbd70b2270176f49c01dbe7ede
Sanae <sanae.s.soma@gmail.com>
2024-07-08 22:16:09 -1000

    create cv directory with Intro.jsx inside

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <Route exact path="/" element={<Intro />} />
       </Routes>
 
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -38,7 +38,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
