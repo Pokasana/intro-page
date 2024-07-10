7061766fedee08b335e52e8d8d7b119d54bf0540
Sanae <sanae.s.soma@gmail.com>
2024-07-09 21:24:29 -1000

    add buttons in info.jsx

--- src/features/cv/Info.jsx
+++ src/features/cv/Info.jsx
@@ -6,9 +6,15 @@ function Info() {
       <h1>Sanae Shiromoto</h1>
       <p>Software Engineer</p>
       <p>Honolulu, Hawaii, United States</p>
-      <a>Mail</a>
-      <span className='i-carbon-logo-github'></span>
-      <button className="i-carbon-light" />
+      <a>
+        <button className="i-carbon-email" />
+      </a>
+      <a>
+        <button className="i-carbon-logo-linkedin" />
+      </a>
+      <a>
+        <button className='i-carbon-logo-github' />
+      </a>
     </div>
   )
 }
