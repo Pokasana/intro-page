00c65dd91eb3a4c0e7bf20e6b8836122dc5874f4
Sanae <sanae.s.soma@gmail.com>
2024-07-12 01:22:08 -1000

    add JSX as the return value of Education component

---
+++ src/features/cv/Education.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Education() {
+  return (
+    <div>
+      <h2>Education</h2>
+      <h3>Flatiron School</h3><span>2024</span>
+      <h4>Software Engineering Flex Course</h4>
+
+      <h3>Osaka University of Arts Junior Collae</h3><span>2006</span>
+      <h4>Visual Design Course</h4>
+    </div>
+  )
+}
+
+export default Education;
