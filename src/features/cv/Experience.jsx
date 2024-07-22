c4f1f9db158c1cd9e4d19c2bcbf6df5fd385cc7d
Sanae <sanae.s.soma@gmail.com>
2024-07-21 23:24:07 -1000

    create Experience.jsx and add primary function

---
+++ src/features/cv/Experience.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Experience() {
+  return (
+    <div>
+      <h2>Experience</h2>
+
+      <h3>Rakuten Travel Hawaii, LCC<span>Honolulu, HI</span></h3><div>Nov 2019 - Oct 2023</div>
+      <h4>Business Operation Assistant</h4>
+
+      <h3>Coty Group HFC Prestige Japan G.K.<span>Tokyo, Japan</span></h3>
+      <h4>Sales Operation Assistant</h4>
+      <div>Feb 2016 - Dec 2018</div>
+      <p>Worked as an assistant for the sales management section. Managed marketing/sales-related tools, and distribution schedule.</p>
+
+
+      <h4>Shared Services Lead</h4>
+      <div>Sep 2015 - Feb 2016</div>
+      <p>Sales administrative tasks, such as managing leases, office facilities, and invoices.</p>
+      
+    </div>
+  )
+};
+
+export default Experience;
