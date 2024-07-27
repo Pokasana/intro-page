46f5dd1b787cc0a7648c27c25b733d5e6407303b
Sanae <sanae.s.soma@gmail.com>
2024-07-26 22:09:12 -1000

    add NavBar styling of position flex

--- src/app/NavBar.jsx
+++ src/app/NavBar.jsx
@@ -4,8 +4,8 @@ import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
-    <div id="nav_bar">
-      <NavLink id="nav_link" to="/intro">
+    <div id="nav_bar" className="flex">
+      <NavLink id="nav_link" to="/">
         sanae.s
       </NavLink>
       <NavLink id="nav_link" to="/cv">
