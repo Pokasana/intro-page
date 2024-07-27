fd6eeea795e7ab54135d6b7cb5154a09e12dbd14
Sanae <sanae.s.soma@gmail.com>
2024-07-26 23:06:30 -1000

    move a part of Info.jsx to NameCard.jsx

--- src/features/cv/Info.jsx
+++ src/features/cv/Info.jsx
@@ -5,19 +5,6 @@ import Experience from './Experience.jsx'
 function Info() {
   return (
     <div>
-      <h1>Sanae Shiromoto</h1>
-      <p>Software Engineer</p>
-      <p>Honolulu, Hawaii, United States</p>
-
-      <a>
-        <button className="i-carbon-email" />
-      </a>
-      <a>
-        <button className="i-carbon-logo-linkedin" />
-      </a>
-      <a>
-        <button className='i-carbon-logo-github' />
-      </a>
       
       <div>
         <Education />
--- src/features/cv/NameCard.jsx
+++ src/features/cv/NameCard.jsx
@@ -3,7 +3,19 @@ import React from 'react';
 function NameCard() {
   return (
     <div>
+      <h1>Sanae Shiromoto</h1>
+      <p>Software Engineer</p>
+      <p>Honolulu, Hawaii, United States</p>
 
+      <a>
+        <button className="i-carbon-email" />
+      </a>
+      <a>
+        <button className="i-carbon-logo-linkedin" />
+      </a>
+      <a>
+        <button className='i-carbon-logo-github' />
+      </a>
     </div>
   )
 };
