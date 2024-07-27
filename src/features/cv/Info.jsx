c93db9e7b5a91544a4629c4895dd511f6b1d13f6
Sanae <sanae.s.soma@gmail.com>
2024-07-26 23:07:35 -1000

    import NameCard component in Info.jsx

--- src/features/cv/Info.jsx
+++ src/features/cv/Info.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import NameCard from './NameCard.jsx'
 import Education from './Education.jsx'
 import Experience from './Experience.jsx'
 
 function Info() {
   return (
     <div>
+
+      <NameCard />
       
       <div>
         <Education />
