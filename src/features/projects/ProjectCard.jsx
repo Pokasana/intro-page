513cb79cb87708f86d6015a527b2d6dbc02b2a8c
Sanae <sanae.s.soma@gmail.com>
2024-11-23 12:48:41 -1000

    add a codepen project in ProjectCard, change styling for a:hover in index.css as !important

--- src/features/projects/ProjectCard.jsx
+++ src/features/projects/ProjectCard.jsx
@@ -3,10 +3,26 @@ import React from 'react';
 function ProjectCard() {
   return (
     <section>
-      <h3>Project xxx</h3>
-      <p>
-        Sample text
-      </p>
+      <h3>CODEPEN</h3>
+      <iframe
+        height="300"
+        style={{ width: '100%' }}
+        scrolling="no"
+        title="Three.js Brain Stretch"
+        src="https://codepen.io/sanaspoon/embed/WNVVzPW?default-tab=html%2Cresult"
+        frameBorder="no"
+        loading="lazy"
+        allowTransparency="true"
+        allowFullScreen="true"
+      >
+        See the Pen{' '}
+        <a href="https://codepen.io/sanaspoon/pen/WNVVzPW">
+          Three.js Brain Stretch
+        </a>{' '}
+        by Sanae Soma (
+        <a href="https://codepen.io/sanaspoon">@sssoma</a>) on{' '}
+        <a href="https://codepen.io">CodePen</a>.
+      </iframe>
     </section>
   )
 };
