dba80c8d6aed8fb0c3eaeb8dadb7726843fa68c5
Sanae <sanae.s.soma@gmail.com>
2024-12-06 13:07:45 -1000

    fix layout for Education

--- src/features/cv/Education.jsx
+++ src/features/cv/Education.jsx
@@ -4,13 +4,17 @@ function Education() {
   return (
     <div>
       <h2>Education</h2>
-      <section >
-        <h3>Flatiron School</h3><span>2024</span>
+      <section className="py-2">
+        <div className="flex justify-between w-full">
+          <h3>Flatiron School</h3><p>2024</p>
+        </div>
         <h4>Software Engineering Flex Course</h4>
       </section>
 
-      <section>
-        <h3>Osaka University of Arts Junior Collae</h3><span>2006</span>
+      <section className="py-2">
+        <div className="flex justify-between w-full">
+          <h3>Osaka University of Arts Junior Collage</h3><p>2006</p>
+        </div>
         <h4>Visual Design Course</h4>
       </section>
 
--- src/features/cv/NameCard.jsx
+++ src/features/cv/NameCard.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 function NameCard() {
   return (
-    <div className="mt-10">
+    <div className="py-10">
       <h1>Sanae Shiromoto</h1>
       <h4>Software Engineer</h4>
       <p>Honolulu, Hawaii, United States</p>
 
       <a>
-        <button className="i-carbon-email" />
+        <button className="i-carbon-email text-gray-4" />
       </a>
       <a>
-        <button className="i-carbon-logo-linkedin" />
+        <button className="i-carbon-logo-linkedin text-gray-4" />
       </a>
       <a>
-        <button className='i-carbon-logo-github' />
+        <button className='i-carbon-logo-github text-gray-4' />
       </a>
     </div>
   )
