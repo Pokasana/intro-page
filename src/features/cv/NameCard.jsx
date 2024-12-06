3f64f7800dd01c4a64d89f9b5400df3a4e7a1fad
Sanae <sanae.s.soma@gmail.com>
2024-12-06 13:20:32 -1000

    add urls to a tags in NameCard

--- src/features/cv/NameCard.jsx
+++ src/features/cv/NameCard.jsx
@@ -7,13 +7,13 @@ function NameCard() {
       <h4>Software Engineer</h4>
       <p>Honolulu, Hawaii, United States</p>
 
-      <a>
+      <a href="mailto:hubbub.cutlets_1y@icloud.com">
         <button className="i-carbon-email text-gray-4" />
       </a>
-      <a>
+      <a href="linkedin.com/in/sanae-soma" target="_blank">
         <button className="i-carbon-logo-linkedin text-gray-4" />
       </a>
-      <a>
+      <a href="https://github.com/Pokasana" target="_blank">
         <button className='i-carbon-logo-github text-gray-4' />
       </a>
     </div>
