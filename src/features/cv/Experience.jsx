fafbb99fcdb988c256c86d83ea9ebb93f636e2bd
Sanae <sanae.s.soma@gmail.com>
2024-12-07 12:10:22 -1000

    move years in Education and Experience for better layout, give more space between Education container and Experience container and delete month from each experience

--- src/features/cv/Experience.jsx
+++ src/features/cv/Experience.jsx
@@ -2,28 +2,28 @@ import React from 'react';
 
 function Experience() {
   return (
-    <div className="py-2 my-0">
+    <div className="py-3 my-0">
       <h2>Experience</h2>
 
       <section className="py-2">
-        <h3>Rakuten Travel Hawaii, LCC<span className="m-5 font-semibold">Honolulu, HI</span></h3>
         <div className="flex justify-between w-full">
-          <h4>Business Operation Assistant</h4>
-          <p>Nov 2019 - Oct 2023</p>
+          <h3>Rakuten Travel Hawaii, LCC<span className="m-5 font-semibold">Honolulu, HI</span></h3>
+          <p>2019 - 2023</p>
         </div>
+        <h4>Business Operation Assistant</h4>
       </section>
 
       <section className="py-2">
         <h3>Coty Group HFC Prestige Japan G.K.<span className="m-5 font-semibold">Tokyo, Japan</span></h3>
         <div className="flex justify-between w-full">
           <h4>Sales Operation Assistant</h4>
-          <p>Feb 2016 - Dec 2018</p>
+          <p>2016 - 2018</p>
         </div>
         <aside className="m-1">Worked as an assistant for the sales management section. Managed marketing/sales-related tools, and distribution schedule.</aside>
 
         <div className="flex mt-5 justify-between w-full">
           <h4>Shared Services Lead</h4>
-          <p>Sep 2015 - Feb 2016</p>
+          <p>2015 - 2016</p>
         </div>
         <aside className="m-1">Sales administrative tasks, such as managing leases, office facilities, and invoices.</aside>
       </section>
