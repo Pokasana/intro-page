a5ab36ef8f003273d66eaf6ccf317c89dcac0d33
Sanae <sanae.s.soma@gmail.com>
2024-12-13 17:27:31 -1000

    add more text in IntroCard, add styling for max width

--- src/features/intro/IntroCard.jsx
+++ src/features/intro/IntroCard.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 function IntroCard() {
   return (
-    <section>
+    <section className="max-w-100">
       <h4>
-        I'm a full-stack software engineer.
+        I'm a software engineer and a cat lover who is from the world of art, beauty, and music. I like to think of myself as someone who bridges creativity with technology. I would like to use my skills to make the comunity and world a better place.
       </h4>
       <h4>
-        I'm detaile oriented, self-motivated and self-improving. Always have a mind set of a learner.
+      With a sharp eye for detail and a natural curiosity, I’m always looking for ways to improve things. I thrive on solving problems, and if I spot an open seam, I’m eager to stitch it up. Programming, to me, is like a conversation with the computer—write the right message, get the right response, and together we can innovate.
+      </h4>
+      <h4>
+      Before I became a software engineer, I was a Japanese teacher, and my love for language still runs deep. I’m fascinated by the magic of communication, whether it’s through spoken words or lines of code. And as a lifelong learner, I’m always exploring new things—there’s always more to discover! 
       </h4>
     </section>
   )
