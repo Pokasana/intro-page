85a40e922fb5729da56b8c271a5feb7dec33760f
Sanae <sanae.s.soma@gmail.com>
2024-12-15 13:00:31 -1000

    add technical skills to each education

--- src/features/cv/Education.jsx
+++ src/features/cv/Education.jsx
@@ -9,7 +9,9 @@ function Education() {
           <h3>Flatiron School</h3><p>2024</p>
         </div>
         <h4>Software Engineering Flex Course</h4>
-        
+        <p className="m-1 text-sm">
+          JavaScript | Python | HTML | CSS | SQL | React | Flask | SQLite | SQLAlchemy
+        </p>
       </section>
 
       <section className="py-2">
@@ -17,6 +19,9 @@ function Education() {
           <h3>Osaka University of Arts Junior Collage</h3><p>2006</p>
         </div>
         <h4>Visual Design Course</h4>
+        <p className="m-1 text-sm">
+         Premier Pro | Final Cut | Illustrator | Photoshop
+        </p>
       </section>
 
     </div>
