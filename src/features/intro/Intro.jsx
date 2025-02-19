88f74549380ab0d70ebcec296d4534343a397331
Sanae <sanae.s.soma@gmail.com>
2025-02-19 13:46:37 -1000

    delete underline text decoration from links and add it as styling only for mobile device

--- src/features/intro/Intro.jsx
+++ src/features/intro/Intro.jsx
@@ -3,7 +3,7 @@ import IntroCard from './IntroCard.jsx'
 
 function Intro() {
   return (
-    <div  className="mx-auto w-full print:space-y-6">
+    <div  className="py-10 mx-auto w-full print:space-y-6">
       <h1>
         Hi, I'm Sanae.
       </h1>
---
+++ src/features/writings/Blog.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+function Blog() {
+  return (
+    <div className="py-10">
+      <h1>
+        Technical Writing
+      </h1>
+
+      <div>
+        <ul className="list-none">
+          <li className="py-3">
+            <a 
+            className="hover:font-semibold"
+            href="https://medium.com/@sanae.s.soma/variation-of-validations-9b9929757d10" target="_blank">
+            Variation of validations</a>
+            <br/>
+            <small className="ml-2">Published on May 3, 2024 / Medium</small>
+          </li>
+          <li className="py-3">
+            <a
+            className="hover:font-semibold"
+            href="https://medium.com/@sanae.s.soma/cookies-are-everywhere-what-are-they-are-a9f9ef3d3abe" target="_blank">
+            Cookies are everywhere, what are they?</a>
+            <br/>
+            <small className="ml-2">Published on Mar 27, 2024 / Medium</small>
+          </li>
+          <li className="py-3">
+            <a
+            className="hover:font-semibold"
+            href="https://medium.com/@sanae.s.soma/command-line-interface-cli-2c4efcb5da56" target="_blank">
+            Command Line Interface - CLI</a>
+            <br/>
+            <small className="ml-2">Published on Jan 21, 2024 / Medium</small>
+          </li>
+          <li className="py-3">
+            <a
+            className="hover:font-semibold"
+            href="https://medium.com/@sanae.s.soma/useparams-hook-react-router-7fab77d8bbd2" target="_blank">
+            useParams Hook - React Router</a>
+            <br/>
+            <small className="ml-2">Published on Oct 15, 2023 / Medium</small>
+          </li>
+          <li className="py-3">
+            <a
+            className="hover:font-semibold"
+            href="https://medium.com/@sanae.s.soma/how-to-change-font-weight-on-hover-4f97e7f32a99" target="_blank">
+            How to change font weight on hover</a>
+            <br/>
+            <small className="ml-2">Published on Jun 12, 2023 / Medium</small>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
