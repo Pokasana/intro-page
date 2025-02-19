0d77d941d984ef430e78aac660b7638d1c640112
Sanae <sanae.s.soma@gmail.com>
2025-02-19 01:11:53 -1000

    add margin to icons in NameCard

--- src/features/cv/NameCard.jsx
+++ src/features/cv/NameCard.jsx
@@ -7,13 +7,13 @@ function NameCard() {
       <h4>Software Engineer</h4>
       <p>Honolulu, Hawaii, United States</p>
 
-      <a href="mailto:hubbub.cutlets_1y@icloud.com">
+      <a className="m-2" href="mailto:hubbub.cutlets_1y@icloud.com">
         <button className="i-carbon-email text-gray-4" />
       </a>
-      <a href="https://www.linkedin.com/in/sanae-soma?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BPa6UJoepRQWNiAF1DQxkqg%3D%3D" target="_blank">
+      <a className="m-2" href="https://www.linkedin.com/in/sanae-soma?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BPa6UJoepRQWNiAF1DQxkqg%3D%3D" target="_blank">
         <button className="i-carbon-logo-linkedin text-gray-4" />
       </a>
-      <a href="https://github.com/Pokasana" target="_blank">
+      <a className="m-2" href="https://github.com/Pokasana" target="_blank">
         <button className='i-carbon-logo-github text-gray-4' />
       </a>
     </div>
