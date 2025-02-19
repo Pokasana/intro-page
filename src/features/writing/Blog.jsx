1fb3df590ab6c3b1b5444791615d126520d64b87
Sanae <sanae.s.soma@gmail.com>
2025-02-19 10:31:37 -1000

    delete reading time from each blog post on Blog.jsx and add platform name Medium

--- src/features/writing/Blog.jsx
+++ src/features/writing/Blog.jsx
@@ -15,7 +15,7 @@ function Blog() {
             href="https://medium.com/@sanae.s.soma/variation-of-validations-9b9929757d10" target="_blank">
             Variation of validations</a>
             <br/>
-            <small className="ml-2">Published on May 3, 2024 / 7 min read</small>
+            <small className="ml-2">Published on May 3, 2024 / Medium</small>
           </li>
           <li className="py-3">
             <a
@@ -23,7 +23,7 @@ function Blog() {
             href="https://medium.com/@sanae.s.soma/cookies-are-everywhere-what-are-they-are-a9f9ef3d3abe" target="_blank">
             Cookies are everywhere, what are they?</a>
             <br/>
-            <small className="ml-2">Published on Mar 27, 2024 / 6 min read</small>
+            <small className="ml-2">Published on Mar 27, 2024 / Medium</small>
           </li>
           <li className="py-3">
             <a
@@ -31,7 +31,7 @@ function Blog() {
             href="https://medium.com/@sanae.s.soma/command-line-interface-cli-2c4efcb5da56" target="_blank">
             Command Line Interface - CLI</a>
             <br/>
-            <small className="ml-2">Published on Jan 21, 2024 / 5 min read</small>
+            <small className="ml-2">Published on Jan 21, 2024 / Medium</small>
           </li>
           <li className="py-3">
             <a
@@ -39,7 +39,7 @@ function Blog() {
             href="https://medium.com/@sanae.s.soma/useparams-hook-react-router-7fab77d8bbd2" target="_blank">
             useParams Hook - React Router</a>
             <br/>
-            <small className="ml-2">Published on Oct 15, 2023 / 4 min read</small>
+            <small className="ml-2">Published on Oct 15, 2023 / Medium</small>
           </li>
           <li className="py-3">
             <a
@@ -47,7 +47,7 @@ function Blog() {
             href="https://medium.com/@sanae.s.soma/how-to-change-font-weight-on-hover-4f97e7f32a99" target="_blank">
             How to change font weight on hover</a>
             <br/>
-            <small className="ml-2">Published on Jun 12, 2023 / 4 min read</small>
+            <small className="ml-2">Published on Jun 12, 2023 / Medium</small>
           </li>
         </ul>
       </div>
