e34fb8788a9a24f2518e97828a05121c5636bf2b
Sanae <sanae.s.soma@gmail.com>
2025-02-22 12:38:11 -1000

    add keys to each mapped NavLink

--- src/app/NavBar.jsx
+++ src/app/NavBar.jsx
@@ -4,12 +4,16 @@ import { NavLink } from "react-router-dom";
 
 function NavBar() {
 
-  const navLinks = [{name: 'sanae.s', path: '/'}, {name: 'cv', path: '/cv'}, {name: 'writing', path:'/writing'}];
+  const navLinks = [
+    {name: 'sanae.s', path: '/'},
+    {name: 'cv', path: '/cv'},
+    {name: 'writing', path:'/writing'}
+  ];
 
   return (
     <div className="grid grid-cols-5 text-center">
 
-      {navLinks.map(link => <NavLink className="hover:font-bold" to={link.path}>{link.name}</NavLink>)}
+      {navLinks.map(link => <NavLink key={link.name} className="hover:font-bold" to={link.path}>{link.name}</NavLink>)}
 
     </div>
   );
