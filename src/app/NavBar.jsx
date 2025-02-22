ae7b1b869f1ac1caba734b40cf00fd72c1c59ecb
Sanae <sanae.s.soma@gmail.com>
2025-02-22 12:36:10 -1000

    refacter NavLink rendering with use map on an array of objects

--- src/app/NavBar.jsx
+++ src/app/NavBar.jsx
@@ -4,20 +4,13 @@ import { NavLink } from "react-router-dom";
 
 function NavBar() {
 
+  const navLinks = [{name: 'sanae.s', path: '/'}, {name: 'cv', path: '/cv'}, {name: 'writing', path:'/writing'}];
+
   return (
     <div className="grid grid-cols-5 text-center">
-      <NavLink className="hover:font-bold" to="/">
-        sanae.s
-      </NavLink>
-      {/* <NavLink className="hover:font-bold" to="/projects">
-        projects
-      </NavLink> */}
-      <NavLink className="hover:font-bold" to="/cv">
-        cv
-      </NavLink>
-      <NavLink className="hover:font-bold" to="/writing">
-        writing
-      </NavLink>
+
+      {navLinks.map(link => <NavLink className="hover:font-bold" to={link.path}>{link.name}</NavLink>)}
+
     </div>
   );
 }
