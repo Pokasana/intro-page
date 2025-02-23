cf0e75f050866c4de94f497dad725128f74169ca
Sanae <sanae.s.soma@gmail.com>
2025-02-23 00:44:46 -1000

    change indentation to improve readability

--- src/features/cv/NameCard.jsx
+++ src/features/cv/NameCard.jsx
@@ -2,18 +2,26 @@ import React from 'react';
 
 function NameCard() {
   const icons = [
-    {url: "mailto:hubbub.cutlets_1y@icloud.com",
-      img: "i-carbon-email text-gray-4"},
-    {url: "https://www.linkedin.com/in/sanae-soma?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BPa6UJoepRQWNiAF1DQxkqg%3D%3D",
-      img: "i-carbon-logo-linkedin text-gray-4"},
-    {url: "https://github.com/Pokasana", img: "i-carbon-logo-github text-gray-4"}
+    {
+      url: "mailto:hubbub.cutlets_1y@icloud.com",
+      img: "i-carbon-email text-gray-4"
+    },
+    {
+      url: "https://www.linkedin.com/in/sanae-soma?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BPa6UJoepRQWNiAF1DQxkqg%3D%3D",
+      img: "i-carbon-logo-linkedin text-gray-4"
+    },
+    {
+      url: "https://github.com/Pokasana",
+      img: "i-carbon-logo-github text-gray-4"
+    }
   ];
 
   const renderIcons = icons.map(icon => (
-    <a className="m-2" key={icons.indexOf(icon)} href={icon.url} target="_blank">
-      <button className={icon.img}/>
-    </a>
-    ));
+      <a className="m-2" key={icons.indexOf(icon)} href={icon.url} target="_blank">
+        <button className={icon.img}/>
+      </a>
+    )
+  );
 
   return (
     <div className="py-10">
